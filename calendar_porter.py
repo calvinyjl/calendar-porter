70125a8e3175cf5b136bc6872daa067c01cfc925
Calvin Lu <calvinlu.luc@gmail.com>
2025-09-12 15:11:27 +0100

    Added all schedules to DataFrame

--- calendar_porter.py
+++ calendar_porter.py
@@ -7,21 +7,23 @@ page = requests.get(URL)
 
 # Finding the spreadsheet including the date
 soup = BeautifulSoup(page.content, "html.parser")
-schedule = soup.find("div", class_="spreadsheet")
-if schedule:
-    pass
-else:
+schedules = soup.find_all("div", class_="spreadsheet")
+if schedules is None:
     print("No spreadsheets to read from")
 
-# Separating data into variables
-date = schedule.find("span", class_="labelone")
-table = schedule.find("table", class_="spreadsheet")
-data = [] 
-for row in table.find_all("tr"):
-    row_data = []
-    for cell in row.find_all("td"):
-        row_data.append(cell.text)
+# Separating schedules into arrays
+data = []
+for schedule in schedules:
+    date = schedule.find("span", class_="labelone")
+    table = schedule.find("table", class_="spreadsheet")
+    for row in table.find_all("tr"):
+        row_data = []
+        row_data.append(date.get_text(strip=True))
+        for cell in row.find_all("td"):
+            row_data.append(cell.get_text(strip=True))
     data.append(row_data)
 
 df = pd.DataFrame(data)
 print(df)
+
+
