51e8ba6ef426b55335beed458b944d2e8a003232
Calvin Lu <calvinlu.luc@gmail.com>
2025-09-17 23:55:22 +0100

    Created endings for form handling

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const cors = require('cors');
+const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors())
+
+// Endings
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
+
+app.post('/', (req, res) => {
+    res.send(req.body.studentId)
+})
+
+// Port
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
